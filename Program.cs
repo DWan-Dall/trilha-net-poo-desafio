aeb456c1646f46f02aa24bec689e5ae3bf2566e5
DWan-Dall <dwandall@gmail.com>
2024-05-29 11:24:31 -0300

    Efetuado todas as propostas do desafio de POO.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Mod 01", imei: "9999999", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "7891011", modelo: "Mod 11", imei: "00000001", memoria: 120);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Discord");
